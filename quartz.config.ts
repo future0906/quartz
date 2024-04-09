094088c8dca3b4c41cbcbb3a104718912622089c
Shane Yao <future0906@gmail.com>
2024-04-09 14:34:20 +0800

    Change domain name

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "dg.shaneyao.com",
+    baseUrl: "brain.shaneyao.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
